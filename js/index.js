0acb256e3e76e3deabfaf16c7a3b051988d51bde
UsanaseD <todiddy20@gmail.com>
2021-02-17 11:06:15 +0200

    my latest commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
-$(".max_min_button").click(function () {
+$("#first").click(function () {
     
     if ($(this).html() == "+") {
-        $(".news_body").height(250);
         $(this).html("-");
+        $("#body1").height(250);
     } else {
         $(this).html("+");
-        $(".news_body").height(0);
+        $("#body1").height(0);
+    }
+});
+$("#second").click(function () {
+    
+    if ($(this).html() == "+") {
+        $(this).html("-");
+        $("#body2").height(250);
+    } else {
+        $(this).html("+");
+        $("#body2").height(0);
+    }
+});
+$("#third").click(function () {
+    
+    if ($(this).html() == "+") {
+        $(this).html("-");
+        $("#body3").height(250);
+    } else {
+        $(this).html("+");
+        $("#body3").height(0);
     }
 });
